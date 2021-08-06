49fa062bba9d2365479f902d01711277783c6deb
joaoigm <joaoigm@hotmail.com>
2021-08-05 23:26:09 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/ranking', pathMatch: 'full' },
+  { path: 'ranking', loadChildren: () => import('./ranking/ranking.module').then((m) => m.RankingModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
