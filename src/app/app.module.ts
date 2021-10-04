10fe7288cdd26c7a85d1a792f29cf615134f2d12
joaoigm <joaoigm@hotmail.com>
2021-10-03 23:53:36 -0300

    atualizando pacotes + trazendo css e html do site webflow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
-import { MatSidenavModule } from '@angular/material/sidenav'
 
 @NgModule({
   declarations: [
@@ -13,9 +10,7 @@ import { MatSidenavModule } from '@angular/material/sidenav'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSidenavModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
